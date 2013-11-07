6fbf4b6d9558fc118de5821cba236db4ac68c390
Leonardo Dutra <leodutra.br@gmail.com>
2013-11-07 19:38:21 -0200

    Update logger.js

--- ideas/logger/logger.js
+++ ideas/logger/logger.js
@@ -8,20 +8,19 @@ var Logger = function($) {
 
 			var id = 'web-commons-logger';
 
-			var $box = $('<div style="display: none; position: absolute; top: 10px; right: 10px; width: 400px; height: 300px; z-index: 9999; border: 1px solid #ddd;  font: normal 12px \'Courier New\', Courier, monospace; border: 1px solid #DDD;background: #FFF;"></div>');
+			var $box = $('<div style="display: none; position: absolute; top: 10px; right: 10px; width: 600px; height: 300px; z-index: 9999; border: 1px solid #ddd;  font: normal 12px \'Courier New\', Courier, monospace; border: 1px solid #DDD; background: #FFF;"></div>');
 			$box.css('opacity', 0.93);              
-			           
-			var $close = $('<div style="position: absolute; right: 5px;line-height: 18px;"><b>x</b></div>');
+
+			var $title = $('<div style="position: relative; text-indent: 5px; line-height: 18px; background: #DDD;"><b>'+id+':</b></div>');
+			var $close = $('<div style="position: absolute; top: 0; right: 5px;"><b>x</b></div>');
 			$close.on('click', function() {
 			  that.hide();
 			  that.clear();
 			});
-			$box.append($close);
-
-			var $title = $('<div style="text-indent: 5px; line-height: 18px; background: #DDD;"><b>'+id+':</b></div>');
+			$title.append($close);
 			$box.append($title);
 
-			var $content = $('<div class="log" style="word-wrap: break-word; word-break: break-all; width: 100%;height: 282px; padding: 0 5px;"></div>');
+			var $content = $('<div class="log" style="word-wrap: break-word; word-break: break-all; height: 282px; padding: 0 5px;"></div>');
 			$content.css('overflow-y', 'scroll');
 			$box.append($content);
 
@@ -31,61 +30,70 @@ var Logger = function($) {
 		}
 	}
 
-	function serialize(any) {
-		var colors = this.colors;
+	var colors = {
+		'number': '#099',
+		'string': '#D14',
+		'function': '#009926',
+		'array': '#51A841',
+		'object': '#444',
+		'boolean': '#00F',
+		'undefined': '#444',
+		'null': '#777',
+		'property': '#333',
+		'regexp': '#009926'
+	};
+
+	function stylize(str, useHtml, type) {
+		return useHtml ? '<b style="color: ' +colors[type] +'">' +str +'</b>' : str;
+	}
+
+	function serialize(any, useHtml) {
+
 		var type = $.type(any);
-		var res = '<b style="color: ' +colors[type] +'">';
 		var builder = [];
+		var res;
+
 		switch(type) {
 
 			case 'function':
-				res += any.toString().replace(/\s+/gim, ' ').replace(/\r?\n/gim, '').match(/.+?\)/gim);
+				res = ('' + any).replace(/\s+/gim, ' ').replace(/\r?\n/gim, '').match(/.+?\{/gim)+'}';
 				break;
 
 			case 'array':
 				for (var i = 0, l = any.length; i < l; ++i) {
-					builder.push(serialize(any[i]));
+					builder.push(serialize(any[i], useHtml));
 				}
-				res += '['+ builder.join(', ') +']';
+				res = '['+ builder.join(', ') +']';
 				break;
 
 			case 'object':
 				for (var k in any) {
 					if (any.hasOwnProperty(k)) {
-						builder.push("<b>'" +k +"'</b>: " +serialize(any[k]));
+						builder.push(
+							stylize(k, useHtml, 'property')+': ' +serialize(any[k], useHtml)
+						);
 					}
 				}
-				res += '{'+ builder.join(', ') +'}';
+				res = '{'+ builder.join(', ') +'}';
 				break;
 
 			case 'null':
 			case 'undefined':
-				res += type;
+				res = type;
 				break; 
 
 			case 'string': 
-				res += '"' +any +'"'; 
+				res = '"' +any.replace(/(["'\\])/gim, '\\$1') +'"'; 
 				break; 
 			case 'number':
 			case 'boolean':
-				res += any;
+				res = any;
 				break; 
 		}
-
-		return res + '</b>';
+		return stylize(res, useHtml, type);
 	}
 
 	Logger.prototype = {
-
-		colors = {
-			'number': '#099',
-			'string': '#D14',
-			'function': '#009926',
-			'array': '#0086B3',
-			'object': '#000',
-			'undefined': '#ddd',
-			'null': '#bbb',
-		},
 
 		log: function(/*args*/) {
 			this.$box.show();
@@ -93,12 +101,13 @@ var Logger = function($) {
 			var args = Array.prototype.slice.call(arguments);
 			var res = [];
 			for (var i = 0, l = args.length; i < l; ++i) {
-				res.push(serialize(args[i]));
+				res.push(serialize(args[i], true));
 			}
 
 			var $log = this.$box.children('.log');
-			$log.append('<div>' +res.join(', ') +'</div><br/>');
-			$log.css('scrollTop', $log.height());
+			$log.append('<p style="margin: 0.5em 0;">' +res.join(', ') +'</p>');
+			$log.clearQueue();
+			$log.animate({scrollTop: $log[0].scrollHeight});
 		},
 
 		show: function() {
