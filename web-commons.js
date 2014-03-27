01865580eb0fcfc61295758255af2497aa3313ad
Leonardo Dutra <leodutra.br@gmail.com>
2014-03-27 15:53:21 -0300

    Update web-commons.js

--- web-commons.js
+++ web-commons.js
@@ -1,13 +1,3 @@
-// Hello.
-//
-// This is JSHint, a tool that helps to detect errors and potential
-// problems in your JavaScript code.
-//
-// To start, simply enter some JavaScript anywhere on this page. Your
-// report will appear on the right side.
-//
-// Additionally, you can toggle specific options in the Configure
-// menu.
 
 /**
  * Collection of useful JavaScript snippets, integrated in one open source lib.
