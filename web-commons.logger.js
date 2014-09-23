8eed2f17a91effd42e85d9bfe2948e0a793742ac
Leonardo Dutra <leodutra.br@gmail.com>
2014-09-23 16:04:34 -0300

    Fixed factory

--- web-commons.logger.js
+++ web-commons.logger.js
@@ -2,7 +2,7 @@
 	if (!jQuery) throw 'jQuery is required by web-commons';
 	
 	var bundle = factory(window, jQuery);
-	if (typeof window !== 'undefined') window.web = (window.web || {}).Logger = bundle;
+	if (typeof window !== 'undefined') (window.web || window).Logger = bundle; 
 	else if (typeof module !== 'undefined' && module.exports) module.exports = bundle;
 })
 (function(window, $) {
